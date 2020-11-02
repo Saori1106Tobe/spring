57444f3ebe308a0dc8a643fa2899f958bd9422e3
Saori1106Tobe <saorin.t.11.6@gmail.com>
2020-11-02 13:59:48 +0900

    コミット

---
+++ ecsite/src/main/java/jp/co/internous/ecsite/model/entity/User.java
@@ -0,0 +1,30 @@
+package jp.co.internous.ecsite.model.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="user")
+public class User {
+
+	@Id
+	@Column(name="id")
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(name="user_name")
+	private String userName;
+	
+	@Column(name="password")
+	private String password;
+	
+	@Column(name="full_name")
+	private String fillName;
+	
+	@Column(name="is_admin")
+	private int isAdmin;
+}
